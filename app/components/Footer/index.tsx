496fe33475b08127514a76b9f9536f8dcc319493
Hernan Clich <hernan.clich@wolox.com.ar>
2021-07-09 11:30:56 -0300

    Completed Footer styling

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -8,32 +8,30 @@ import FooterContainer from './styles';
 function Footer() {
   return (
     <FooterContainer>
-      <div>
-        <div>
-          <div>
-            <CustomText size="small" textAlign="left" weight="bold">
-              creacionesmsariablas@hotmail.com
-            </CustomText>
-            <CustomText size="small" textAlign="left" weight="bold">
-              +54 11 5123 4567
-            </CustomText>
-          </div>
-          <div>
-            <CustomText size="small" textAlign="left" weight="regular">
-              Maure 2453, C1426
-            </CustomText>
-            <CustomText size="small" textAlign="left" weight="regular">
-              Ciudad Autónoma de Buenos Aires
-            </CustomText>
-          </div>
+      <div className="footer-text">
+        <div className="footer-text-info">
+          <CustomText as="small" size="small" textAlign="left" weight="bold">
+            creacionesmsariablas@hotmail.com
+          </CustomText>
+          <CustomText as="small" size="small" textAlign="left" weight="bold">
+            +54 11 5123 4567
+          </CustomText>
+        </div>
+        <div className="footer-text-info">
+          <CustomText as="small" size="small" textAlign="left" weight="regular">
+            Maure 2453, C1426
+          </CustomText>
+          <CustomText as="small" size="small" textAlign="left" weight="regular">
+            Ciudad Autónoma de Buenos Aires
+          </CustomText>
         </div>
         <div>
-          <CustomText size="small" textAlign="left" weight="regular">
+          <CustomText as="small" size="small" textAlign="left" weight="regular">
             © 2021 - Rayuela Uniformes
           </CustomText>
         </div>
       </div>
-      <div>
+      <div className="footer-social">
         <WaIcon />
         <FbIcon />
         <EmailIcon />
--- app/components/Footer/styles.ts
+++ app/components/Footer/styles.ts
@@ -5,10 +5,58 @@ import { PRIMARY_SPACING_EM } from '@styles/variables';
 const FooterContainer = styled.div`
   background-color: ${ALABASTER};
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: row;
   margin: 0 auto ${PRIMARY_SPACING_EM};
   min-height: 50vh;
+  padding: ${PRIMARY_SPACING_EM};
   width: calc(100vw - ${PRIMARY_SPACING_EM} * 2);
+
+  .footer-text,
+  .footer-social {
+    flex-grow: 1;
+  }
+
+  .footer-text {
+    small {
+      display: block;
+    }
+  }
+
+  .footer-text-info {
+    padding-bottom: ${PRIMARY_SPACING_EM};
+  }
+
+  .footer-social {
+    display: flex;
+    height: auto;
+    justify-content: flex-end;
+
+    svg {
+      margin: 0 0.75em;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
+    padding: 0 0 0 0.5em;
+
+    .footer-text {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+
+      small {
+        text-align: center;
+      }
+    }
+
+    .footer-social {
+      flex-grow: 0;
+      height: auto;
+      justify-content: center;
+      padding: 2em 0;
+    }
+  }
 `;
 
 export default FooterContainer;
