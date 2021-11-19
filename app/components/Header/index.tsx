ca4fa830e8e72de53ccb64b1a3d36a19f690563b
hernan-clich <hernan.g.clich@gmail.com>
2021-11-19 00:47:26 -0300

    Removed ugly nav from Header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,29 +1,14 @@
 import Link from 'next/link';
-import CustomText from '~components/CustomText';
 import PATHS from '~constants/paths';
-import useWindowSize from '~hooks/useWindowSize';
-import { SMALL_BR } from '~styles/variables';
 import Logo from './components/Logo';
 import ShoppingCartIcon from './components/ShoppingCartIcon';
 import * as Styled from './styles';
 
 function Header() {
-  const { width } = useWindowSize();
-
   return (
     <Styled.HeaderContainer>
       <Styled.HeaderLeft>
         <Logo />
-        {width >= SMALL_BR && (
-          <Styled.NavContainer>
-            <CustomText as="span" size="small" textTransform="uppercase" weight="bold">
-              Colegios
-            </CustomText>
-            <CustomText as="span" size="small" textTransform="uppercase" weight="bold">
-              Categorías
-            </CustomText>
-          </Styled.NavContainer>
-        )}
       </Styled.HeaderLeft>
       <Link href={PATHS.CART}>
         <a>
