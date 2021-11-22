0b4b1b728283b3b278bb8c07dcf1dac92924adfa
hernan-clich <hernan.g.clich@gmail.com>
2021-11-22 11:43:54 -0300

    Styled PrivateLayout ChildrenContainer

--- app/components/PrivateLayout/styles.ts
+++ app/components/PrivateLayout/styles.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { WILD_SAND } from '~styles/colors';
 
 export const PrivateLayoutContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
 `;
 
-export const ChildrenContainer = styled.div``;
+export const ChildrenContainer = styled.div`
+  background-color: ${WILD_SAND};
+  border-radius: 10px;
+  height: calc(100vh - 2em);
+  margin: 1em 1em 1em 0;
+  width: calc(100vw - 90px - 2em);
+`;
