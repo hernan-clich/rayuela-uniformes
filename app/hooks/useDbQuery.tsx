8f292b8ac510be587e2aa4858c52aca9e2f99e3e
hernan-clich <hernan.g.clich@gmail.com>
2021-12-12 17:25:17 -0300

    Now receiving the expected data type in useDbQuery

--- app/hooks/useDbQuery.tsx
+++ app/hooks/useDbQuery.tsx
@@ -3,15 +3,15 @@ import { useEffect, useState } from 'react';
 import { db } from '~config/firebase/client';
 import { EDbCollections, TDbCollections } from '~types/db';
 
-function useDbQuery(collectionName: TDbCollections) {
-  const [data, setData] = useState<unknown[]>([]);
+function useDbQuery<T extends { id: string }>(collectionName: TDbCollections): [T[]] {
+  const [data, setData] = useState<T[]>([]);
   const colRef = collection(db, EDbCollections[collectionName]);
   const dbQuery = query(colRef);
 
   useEffect(
     () =>
       onSnapshot(dbQuery, (snapshot) => {
-        setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as unknown as T)));
       }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
