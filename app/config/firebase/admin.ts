73ea7a55c02120fcd5dc4986ae064d8cbcef6ff0
hernan-clich <hernan.g.clich@gmail.com>
2022-02-06 18:17:22 -0300

    Stringified admin key

--- app/config/firebase/admin.ts
+++ app/config/firebase/admin.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import type { ServiceAccount } from 'firebase-admin';
-import * as serviceAccount from './serviceAccount.json';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
 const initializeAdminApp = () => {
   if (!admin.apps.length) {
